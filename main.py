7d6eef3751f6b92d5424b2dcfb8747375a5c0ac1
Asama3 <rglcybrtem@gmail.com>
2024-09-23 06:53:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+def harf_ekranda_yazmiyor():
+    harf = a
+    print(harf)
+    #harfi nasıl çıktı alabilirim, çıktı aldığın harf flag'in cevabı olacaktır (Basit Düşün :=)... 
+    #devamı için adminstrator.html hesabına erişim sağlamalısın :)
+harf_ekranda_yazmiyor()
